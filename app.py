536fab27e018e9e4296ebe5d06f838d661fda889
alexschanne <alex.schanne@gmail.com>
2020-11-20 00:44:48 -0500

    changing the file structure for the flask app

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 # Import Dependencies
-
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import pymongo
+import coin_scraping
+import os 
 import sqlalchemy
 import json
 
@@ -8,12 +11,29 @@ import json
 
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = "mongodb://localhost:27017/bitcoin_app"
+mongo = PyMongo(app)
+
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+
+# set database 
+db=client.mars_app
+
 @app.route("/")
-def init():
+#This route can be edited to reflect machine learning addition
+def index():
     return render_template("index.html")
 
-
-
+@app.route("/news")
+def scrape():
+    coin = mongo.db.bitcoin_dict
+    coin_data = coin_scraping.scrape()
+    
+    #Updating Mongodb using update and upsert
+    coin.update({}, coin_data, upsert=True)
+    
+    return redirect("/", code = 302)
 
 if __name__ == '__main__':
     app.run(debug=True)
